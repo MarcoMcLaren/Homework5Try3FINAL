9d2752dde0b57ab416f47855713efe33d6d3b4fe
Marco Mac <mclarenmarco998@gmail.com>
2022-09-30 17:43:55 +0200

    Add project files.

---
+++ Homework5Try3/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using Homework5Try3.Models;
+using Service = Homework5Try3.Models.Service;
+
+namespace Homework5Try3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Service dataService = new Service();
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<books> books = dataService.getAllBooks();  
+            return View(books);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchtext, string author,string type)
+        {
+            List<books> books = dataService.getAllBooks().Where(x => x.name.Contains(searchtext)).ToList();
+            return View(books);
+        }
+    }
+}
